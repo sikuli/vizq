7250be1b39719053c7e141381cff011ee770103c
Tom Yeh <doubleshow@gmail.com>
2015-09-18 10:59:00 -0600

    dsl init implementation; precision, floor, round

--- test/eval.js
+++ test/eval.js
@@ -9,46 +9,51 @@ function test(q){
     console.log(JSON.stringify(result,null, ' '))
 }
 
-test({
-    Subject: 'col',
-    CrsLvlNum: 'col'
-})
+// test({
+//     Subject: 'col',
+//     CrsLvlNum: 'col'
+// })
+//
+// test({
+//     Subject: 'col',
+//     Hours: 'col'
+// })
+//
+// test({
+//     CrsLvlNum: 'col',
+//     Subject: 'col'
+// })
+//
+// test({
+//     Subject: 'col',
+//     AVG_GRD: 'floor | col'
+// })
+//
+// test({
+//     Subject: 'col',
+//     AVG_GRD: 'precision 0.5 | col'
+// })
+//
+// test({
+//     Subject: 'col',
+//     AVG_GRD: 'floor | col'
+// })
+//
+// test({
+//     Subject: 'col',
+//     N: {
+//         ENROLL: 'precision 10 | col'
+//     }
+// })
+//
+// test({
+//     Subject: 'col',
+//     Workload: {
+//         Raw: 'round | col'
+//     }
+// })
+
 
 test({
-    Subject: 'col',
-    Hours: 'col'
-})
-
-test({
-    CrsLvlNum: 'col',
     Subject: 'col'
-})
-
-test({
-    Subject: 'col',
-    AVG_GRD: 'floor | col'
-})
-
-test({
-    Subject: 'col',
-    AVG_GRD: 'precision 0.5 | col'
-})
-
-test({
-    Subject: 'col',
-    AVG_GRD: 'floor | col'
-})
-
-test({
-    Subject: 'col',
-    N: {
-        ENROLL: 'precision 10 | col'
-    }
-})
-
-test({
-    Subject: 'col',
-    Workload: {
-        Raw: 'round | col'
-    }
 })
